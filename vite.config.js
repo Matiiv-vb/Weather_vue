ac04d5b0ad825945846b7a199d4f1601a8bd761e
Matiiv-vb <vasya.matiyiv@gmail.com>
2023-06-07 11:09:28 +0300

    add env

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
 
+const ASSET_URL = process.env.ASSET_URL || '';
+
+
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
@@ -10,5 +14,5 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: "/Weather_vue/",
+  base: `${ASSET_URL}`,
 });
